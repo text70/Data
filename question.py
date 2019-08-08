7c8889fc345437a98db184567db87f736fbc25ba
text70 <31259119+text70@users.noreply.github.com>
2019-08-08 11:57:15 -0500

    Add files via upload

---
+++ question.py
@@ -0,0 +1,26 @@
+#text70/# question.py
+#8/8/2019
+import click
+import re
+
+
+@click.command()
+@click.option('--question', prompt='Are you a robot? [y/n]',
+                help='The ultimate question.')
+
+
+def robot(question):
+    """The answer to life itself and everything"""
+    if (question == str('y')):
+        click.echo('Woo-hoo a human')
+    if (question == str('n')):
+        click.echo("Beep-boop")
+    if (question == str(42)):
+        click.echo("A true believer")
+    else:
+        while question:
+            click.echo("Try again")
+            break
+
+if __name__ == '__main__':
+    robot()
